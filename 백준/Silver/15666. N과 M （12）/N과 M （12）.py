8267b3d3cfbdb3315bb5132e36ab908401cd6cc9
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2024-10-03 23:56:50 +0900

    [Silver II] Title: N과 M (12), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+
+lst = list(map(int, input().split()))
+lst.sort()
+
+visited = [False]* N
+answer = []
+def back(x):
+    prev = 0
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
+        return
+
+    for i in range(x, N):
+        if prev != lst[i]:
+            answer.append(lst[i])
+            prev = lst[i]
+            back(i)
+            answer.pop()
+
+back(0)
