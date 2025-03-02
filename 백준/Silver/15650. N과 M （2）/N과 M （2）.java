d03f1f53a2afc1e5b357e9ff0c36214fb28b950f
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-03-02 21:28:44 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,42 +1,43 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int n, m;
-	static boolean[] visited;
-	static int[] arr;
+    static int N, M;
+    static int[] arr;
+  //  static boolean[] visited;
 
-	static void dfs(int depth, int start) {
-		if (depth == m) {
-			for (int i = 0; i < m; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
 
-		for (int i = start; i < n; i++) {
-			if (!visited[i]) {
-				arr[depth] = i + 1;
-				visited[i] = true;
-				dfs(depth + 1, i + 1);
-				visited[i] = false;
-			}
-		}
-	}
+        for (int i = start; i < N; i++) {
+            // if (!visited[i]) {
+            //  visited[i] = true;
+            arr[depth] = i + 1;
+            dfs(depth + 1, i + 1);
+            //  visited[i] = false;
+        }
 
-	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
 
-		String s[] = br.readLine().split(" ");
-		n = Integer.parseInt(s[0]);
-		m = Integer.parseInt(s[1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
 
-		visited = new boolean[n];
-		arr = new int[m];
+        //visited = new boolean[N];
+        arr = new int[M];
+        dfs(0, 0);
 
-		dfs(0, 0);
-	}
+    }
+
 }
