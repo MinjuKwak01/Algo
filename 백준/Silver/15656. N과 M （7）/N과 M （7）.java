466d9a13dcb957cac91fa25742d6ea8317ceac70
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-03-02 22:41:47 +0900

    [Silver III] Title: N과 M (7), Time: 544 ms, Memory: 219980 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] newArr;
+    //    static boolean[] visited;
+    static StringBuilder sb;
+
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(newArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0 ; i < N; i++) {
+            newArr[depth] = arr[i];
+            dfs(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        sb = new StringBuilder();
+        s = br.readLine().split(" ");
+        arr = new int[N];
+        newArr = new int[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.print(sb);
+    }
+}
