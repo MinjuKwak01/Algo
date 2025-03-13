eb9823fe5387203d68f680611a73aae6d153b247
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-03-13 14:26:07 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,7 @@
-SELECT
-    NAME,
-    DATETIME
-FROM
-    ANIMAL_INS
-WHERE
-    ANIMAL_ID NOT IN (SELECT ANIMAL_ID FROM ANIMAL_OUTS)
-ORDER BY
-    DATETIME ASC
-LIMIT 3;
+-- 코드를 입력하세요
+SELECT i.name as NAME, i.datetime as DATETIME
+from animal_ins i
+left outer join animal_outs o
+on i.animal_id = o.animal_id
+order by i.datetime
+limit 3
