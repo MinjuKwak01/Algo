9cc5e19301f8898764cbef4146e63f6b61beea09
Minju Kwak <105683527+MinjuKwak01@users.noreply.github.com>
2025-05-13 23:38:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 233 ms, Memory: 29,616 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -2,47 +2,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    static int T, N;
+    static int T;
+    static int N;
     static int[][] arr;
     static boolean[] visited;
-    static int minimum;
+    static int res;
 
-    static void dfs(int cnt, int start) {
-        if(cnt == N / 2) { //절반 고름
-          //  System.out.println(Arrays.toString(visited));
-            int sum1 = 0, sum2 = 0;
-
-            for(int i = 0; i < N; i++) {
-                for(int j = i + 1; j < N; j++){
-                    if(visited[i] && visited[j]) {
-                        sum1 += arr[i][j] + arr[j][i]; // 고른 식재료로 만든 음식
-                    } else if(!visited[i] && !visited[j]) {
-                        sum2 += arr[i][j] + arr[j][i]; // 나머지 식재료로 만든 음식
-                    }
-                }
-            }
-            int diff = Math.abs(sum1 - sum2);	// 맛의 차이
-            minimum = Math.min(minimum, diff);		// 최솟값 비교
+    static void dfs(int depth, int cnt) {
+        if (depth == N / 2) {
+            check();
             return;
         }
-        for(int i = start; i < N; i++) {
+        for (int i = cnt; i < N; i++) {
             visited[i] = true;
-            dfs(cnt+1, i+1);
+            dfs(depth + 1, i + 1);
             visited[i] = false;
+
         }
+    }
+
+    static void check() {
+        int sumA = 0;
+        int sumB = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (visited[i] && visited[j]) {
+                    sumA += arr[i][j] + arr[j][i];
+                } else if (!visited[i] && !visited[j]) {
+                    sumB += arr[i][j] + arr[j][i];
+                }
+            }
+        }
+
+        int min = Math.abs(sumA - sumB);
+        res = Math.min(res, min);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        for(int t=0; t<T; t++){
+
+        for(int tc=1; tc<=T; tc++){
             N = Integer.parseInt(br.readLine());
-            minimum = Integer.MAX_VALUE;
-            visited = new boolean[N];
             arr = new int[N][N];
+            visited = new boolean[N];
+            res = Integer.MAX_VALUE;
             for (int i = 0; i < N; i++) {
                 String[] s = br.readLine().split(" ");
                 for (int j = 0; j < N; j++) {
@@ -50,7 +59,8 @@ public class Solution {
                 }
             }
             dfs(0, 0);
-            System.out.println("#" + (t+1) + " " + minimum);
+            System.out.println("#" + tc + " "  + res);
+
         }
 
     }
